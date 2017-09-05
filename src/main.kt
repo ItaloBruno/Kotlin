5c99c206a0fbb81745473ed2db97965cf305495e
ItaloBruno <italobruno_207@hotmail.com>
2017-09-05 12:26:04 -0300

    Primeiro programa em Kotlin

---
+++ src/main.kt
@@ -0,0 +1,11 @@
+fun minhaPrimeiraFuncao(nome: String = "Visitante", idade: Int = 15)
+{
+    print(nome)
+    print(". Tenho "+ idade + " anos")
+}
+
+fun main(args: Array <String>)
+{
+    print("Eae ")
+    minhaPrimeiraFuncao()
+}
